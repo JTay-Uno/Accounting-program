41155a85d2ab820c6ab4d6eecafa63d82e8478ea
JTay-Uno <jttlr33@gmail.com>
2024-12-13 22:22:55 -0600

    Add files via upload

--- logic.py
+++ logic.py
@@ -1,4 +1,3 @@
-
 from PyQt6.QtWidgets import *
 from gui import *
 from accounts import *
@@ -6,20 +5,33 @@ import csv
 import os
 import re
 
+
 class Logic(QMainWindow, Ui_MainWindow):
+    """
+    Logic class for the GUI
+    """
+
     def __init__(self):
+        """
+        Method that sets the default values, hides GUI widgets, and connects widgets to methods for the GUI Logic
+        """
         super().__init__()
         self.setupUi(self)
         self.__account = None
         self.__savings = None
-        self.submit_pushButton.clicked.connect(lambda : self.submit())
-        self.enter_pushButton.clicked.connect(lambda : self.enter())
+        self.submit_pushButton.clicked.connect(lambda: self.submit())
+        self.enter_pushButton.clicked.connect(lambda: self.enter())
         self.exit_pushButton.clicked.connect(self.close)
         self.account_frame.hide()
         self.checking_Radio.hide()
         self.savings_Radio.hide()
 
     def submit(self):
+        """
+        Method that processes firstname, lastname and pin entered by the user. Once valid entries are made, Account and SavingAccount classes
+            are created with the data. The .validate is used to verify if the accounts exist.
+        The appropriate account type radio buttons and withdraw or deposit radio buttons are hidden based on if either Account or SavingAccount was validated
+        """
         try:
             fname = self.fname_Entry.text().strip()
             lname = self.lname_Entry.text().strip()
@@ -46,7 +58,7 @@ class Logic(QMainWindow, Ui_MainWindow):
                 self.fname_Entry.setFocus()
                 self.pin_Entry.clear()
 
-            if self.__account.validate() and self.__savings.validate(): #editing/showing/checking savings_Radio/checking_Radio where appropriate
+            if self.__account.validate() and self.__savings.validate():  # editing/showing/checking savings_Radio/checking_Radio where appropriate
                 self.savings_Radio.setText(f'{self.__savings}')
                 self.savings_Radio.show()
                 self.checking_Radio.setText(f'{self.__account}')
@@ -61,7 +73,9 @@ class Logic(QMainWindow, Ui_MainWindow):
                 self.checking_Radio.setChecked(True)
 
     def enter(self):
-
+        """
+        This method contains the logic for the enter button while checking the states of the radio buttons and submitted amount
+        """
         try:
             amount = float(self.amount_Entry.text())
             if amount <= 0: raise ValueError
@@ -77,7 +91,7 @@ class Logic(QMainWindow, Ui_MainWindow):
                 self.output_text_Label.setText("Please select an account.")
                 return
 
-            states = self.account_buttonGroup.checkedId()   # = -2 for Checking, = -3 for Savings
+            states = self.account_buttonGroup.checkedId()  # = -2 for Checking, = -3 for Savings
             match self.action_buttonGroup.checkedButton().text(), states:
                 case 'Deposit', -2:
                     print("deposit - checking")
@@ -89,17 +103,17 @@ class Logic(QMainWindow, Ui_MainWindow):
                     if self.__savings.deposit(amount):
                         self.savings_Radio.setText(f'{self.__savings}')
                         self.output_text_Label.setText(f'Successfully deposited ${amount:.2f} into savings account.')
-                case 'Withdraw', -2:            # withdraw - checking
+                case 'Withdraw', -2:  # withdraw - checking
                     if self.__account.withdraw(amount):
                         self.checking_Radio.setText(f'{self.__account}')
                         self.output_text_Label.setText(f'Successfully withdrew ${amount:.2f} out of checking account.')
-                case 'Withdraw', -3:            # withdraw - savings
+                case 'Withdraw', -3:  # withdraw - savings
                     if self.__savings.withdraw(amount):
-                            print("withdraw savings")
-                            self.savings_Radio.setText(f'{self.__savings}')
-                            self.output_text_Label.setText(f'Successfully withdrew ${amount:.2f} out of savings account.')
+                        self.savings_Radio.setText(f'{self.__savings}')
+                        self.output_text_Label.setText(f'Successfully withdrew ${amount:.2f} out of savings account.')
                     else:
-                        self.output_text_Label.setText("Please enter a value that will maintain a minimum balance of $100")
+                        self.output_text_Label.setText(
+                            "Please enter a value that will maintain a minimum balance of $100")
                         self.amount_Entry.clear()
                         self.amount_Entry.setFocus()
                 case _:
